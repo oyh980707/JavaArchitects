954ccf343557d1640b342f5ea5aace692f734796
oyh980707 <1135219066@qq.com>
2020-03-01 11:40:51 +0800

    Decorator Pattern, Observer Pattern

---
+++ DesignPatterns/src/main/java/com/loveoyh/ObserverPattern/events/mouseevent/Keybord.java
@@ -0,0 +1,18 @@
+package com.loveoyh.ObserverPattern.events.mouseevent;
+
+import com.loveoyh.ObserverPattern.events.core.EventLisenter;
+
+/**
+ * Created by Tom.
+ */
+public class Keybord extends EventLisenter {
+
+    public void down(){
+
+    }
+
+    public void up(){
+
+    }
+
+}
