2c57b4bff089baa4ac8b279497dfc674e7e618d1
oyh.jerry <1135219066@qq.com>
2022-11-22 23:18:29 +0800

    add

---
+++ OS/code/device/timer.c
@@ -0,0 +1,36 @@
+#include "timer.h"
+#include "io.h"
+#include "print.h"
+
+#define IRQ0_FREQUENCY 100 // 设置的时钟频率 100HZ，也就是IRQ0引脚上的时钟中断信号 每秒钟100次中断
+#define INPUT_FREQUENCY 1193180 // 计数器0的工作脉冲信号频率
+#define COUNTER0_VALUE INPUT_FREQUENCY / IRQ0_FREQUENCY // 计数器0的初始值
+#define COUNTER0_PORT 0x40 // 计数器0的端口号0x40
+#define COUNTER0_NO 0 // 控制字中选择计数器的号码，表示计数器0
+#define COUNTER0_MODE 2 // 计数器0的工作方式，比率发生器
+#define READ_WRITE_LATCH 3 // 读写方式，表示先读写低8位，在读写高8位
+#define PIT_CONTROL_PORT 0x43 // 控制字寄存器的端口
+
+// 把操作的计数器counter_no，读写锁属性rwl，计数器模式counter_mode写入模式控制寄存器并赋予初始值counter_value
+static void frequency_set(uint8_t counter_port, \
+							uint8_t counter_no, \
+							uint8_t rwl, \
+							uint8_t counter_mode, \
+							uint16_t counter_value) {
+	// 先写控制字
+	// 7~6 指定计数器，5～4 读写方式，3～1 工作方式，最后一位进制格式0二进制 1BCD码
+	outb(PIT_CONTROL_PORT, (uint8_t)(counter_no << 6 | rwl << 4 | counter_mode << 1));
+	// 在写counter_value值的低8位
+	outb(counter_port, (uint8_t)(counter_value));
+	// 在写counter_value值的高8位
+	outb(counter_port, (uint8_t)(counter_value >> 8));
+}
+
+void timer_init() {
+	put_str("timer_init start\n");
+	frequency_set(COUNTER0_PORT, COUNTER0_NO, READ_WRITE_LATCH, COUNTER0_MODE, COUNTER0_VALUE);
+	put_str("timer_init done\n");
+}
+
+
+				
