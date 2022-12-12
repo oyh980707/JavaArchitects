1c9986913fcb08cb6f23a47d74ccd6d64032bf17
oyh.jerry <1135219066@qq.com>
2022-12-12 13:13:28 +0800

    add

--- OS/code/kernel/interrupt.c
+++ OS/code/kernel/interrupt.c
@@ -54,7 +54,7 @@ static void pic_init(void) {
 //   outb (PIC_M_DATA, 0xfe);
 //   outb (PIC_S_DATA, 0xff);
 
-    outb(PIC_M_DATA, 0xfd);
+    outb(PIC_M_DATA, 0xfc);
     outb(PIC_S_DATA, 0xff);
 
    put_str("   pic_init done\n");
--- OS/code/kernel/main.c
+++ OS/code/kernel/main.c
@@ -1,10 +1,11 @@
 #include "print.h"
 #include "init.h"
-#include "debug.h"
 #include "memory.h"
 #include "thread.h"
 #include "interrupt.h"
 #include "console.h"
+#include "ioqueue.h"
+#include "keyboard.h"
 
 void k_thread_a(void*);
 void k_thread_b(void*);
@@ -18,8 +19,8 @@ int main(void) {
     put_int((uint32_t)addr);
     put_str("\n");
 
-//    thread_start("k_thread_a", 5, k_thread_a, "argA ");
-//    thread_start("k_thread_b", 5, k_thread_b, "argB ");
+    thread_start("k_thread_a", 5, k_thread_a, "argA_");
+    thread_start("k_thread_b", 5, k_thread_b, "argB_");
 
     put_str("open interrupt ===== start schedule");
     intr_enable(); // 打开中断使时钟中断起作用
@@ -31,17 +32,29 @@ int main(void) {
     return 0;
 }
 
-//void k_thread_a(void * arg){
-//    char * param = arg;
-//    while(1) {
-//        console_put_str(param);
-//    }
-//}
-//
-//void k_thread_b(void * arg){
-//    char * param = arg;
-//    while(1) {
-//        console_put_str(param);
-//    }
-//}
+void k_thread_a(void * arg){
+    char * param = arg;
+    while(1) {
+        enum intr_status old_status = intr_disable();
+        if(!ioq_empty(&kbd_buf)) {
+            console_put_str(param);
+            char byte = ioq_getchar(&kbd_buf);
+            console_put_str(byte);
+        }
+        intr_set_status(old_status);
+    }
+}
+
+void k_thread_b(void * arg){
+    char * param = arg;
+    while(1) {
+        enum intr_status old_status = intr_disable();
+        if(!ioq_empty(&kbd_buf)) {
+            console_put_str(param);
+            char byte = ioq_getchar(&kbd_buf);
+            console_put_str(byte);
+        }
+        intr_set_status(old_status);
+    }
+}
 
