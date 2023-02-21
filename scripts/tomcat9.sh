3c7ad30b06edeed44db1930d0a05c6f8142a1c5d
oyh.jerry <1135219066@qq.com>
2023-02-21 23:47:01 +0800

    add

---
+++ scripts/tomcat9.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /usr/local
+
+wget https://mirrors.cnnic.cn/apache/tomcat/tomcat-9/v9.0.33/bin/apache-tomcat-9.0.33.tar.gz
+
+tar -zxvf apache-tomcat-9.0.33.tar.gz
+
+mv apache-tomcat-9.0.33 tomcat9
+
+
+
+
+
+
