279477cc9b213897706ec07c051fc7a6e02cc998
hgr <1135219066@qq.com>
2023-06-23 14:02:07 +0800

    fix

--- scripts/ubuntu/tomcat9.sh
+++ scripts/ubuntu/tomcat9.sh
@@ -4,9 +4,9 @@ echo 'auto install tomcat9 begining...'
 
 installDir=/opt/soft/tomcat9
 
-sudo wget -P "$installDir" https://mirrors.cnnic.cn/apache/tomcat/tomcat-9/v9.0.33/bin/apache-tomcat-9.0.33.tar.gz
+sudo wget -P "$installDir" https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.76/bin/apache-tomcat-9.0.76.tar.gz
 
-tar -zxf "$installDir"/apache-tomcat-9.0.33.tar.gz -C "$installDir" --strip-components 1
+tar -zxf "$installDir"/apache-tomcat-9.0.76.tar.gz -C "$installDir" --strip-components 1
 
 
 
