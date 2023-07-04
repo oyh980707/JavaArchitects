6046c0748b817933af10b4ebc63ea3458bd6e66b
hgr <1135219066@qq.com>
2023-07-04 21:16:11 +0800

    fix

--- scripts/ubuntu/maven.sh
+++ scripts/ubuntu/maven.sh
@@ -4,7 +4,7 @@ echo 'auto install Tomcat9 begining...'
 
 installDir=/opt/soft/Maven3
 
-sudo wget -P "$installDir" https://dlcdn.apache.org/maven/maven-3/3.9.2/binaries/apache-maven-3.9.2-bin.tar.gz
+sudo wget -P "$installDir" https://dlcdn.apache.org/maven/maven-3/3.9.3/binaries/apache-maven-3.9.3-bin.tar.gz
 
 tar -zxf "$installDir"/apache-maven-3.9.2-bin.tar.gz -C "$installDir" --strip-components 1
 
